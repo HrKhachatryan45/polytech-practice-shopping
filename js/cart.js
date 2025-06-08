2ef9f2cd6abf7fad59fe377c198ff1afcf4cd3e2
Your Name <you@example.com>
2025-06-08 23:37:49 +0400

    added new

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,14 @@
-window.addEventListener('DOMContentLoaded',() => {
+
+
+const getCartElements = () => {
     const cartItems = JSON.parse(localStorage.getItem('cart') || [])
 
     const cartContainer = document.getElementById('cart')
+    cartContainer.innerHTML = ''
 
     cartItems.forEach(element => {
 
         const div =  document.createElement('div')
-        div.innerHTML = ''
         div.className = 'cart-item'
         div.innerHTML = `<img src='../${element.image}' />
         <section>
@@ -14,11 +16,11 @@ window.addEventListener('DOMContentLoaded',() => {
             <h3><span class='${element.color}'></span>${element.color}</h3>
         </section>
         <div>
-        <i id='removeIcon' class="fa-solid fa-trash-can"></i>
+        <i id='removeIcon' onclick='handleRemoveItem(${element.id})' class="fa-solid fa-trash-can"></i>
           <section>
-            <button>+</button>
+            <button>-</button>
             <input type='number' value='1'/>
-             <button>-</button>
+             <button>+</button>
           </section>
           <h1>${element.price}$</h1>
         </div>
@@ -26,5 +28,17 @@ window.addEventListener('DOMContentLoaded',() => {
         cartContainer.appendChild(div)
     });
 
+}
+window.addEventListener('DOMContentLoaded',getCartElements)
 
+
+const handleRemoveItem = (id) => {
+  let cart = JSON.parse(localStorage.getItem('cart') || []);
+  cart = cart.filter((productS) => productS.id != id)
+  localStorage.setItem('cart',JSON.stringify(cart))
+  window.dispatchEvent(new Event('cart-changed'))
+}  
+
+window.addEventListener('cart-changed',() => {
+    getCartElements()
 })
--- js/universal.js
+++ js/universal.js
@@ -14,3 +14,15 @@ const handleQuerySearch = (event) => {
   const value = document.getElementById('someID').value
     window.location.href = 'products.html?searchQuery=' + value
 }    
+
+    const updatePin = () => {
+        const cartItems = JSON.parse(localStorage.getItem('cart') || [])
+        console.log(cartItems);
+            
+        document.getElementsByClassName('quantity')[0].innerHTML = cartItems.length
+    }
+
+      window.addEventListener('load',updatePin)
+
+      window.addEventListener('cart-updated',updatePin)
+      window.addEventListener('cart-changed',updatePin)
